c228097413eadb136c1118bc4ce5b7984b7208de
Nguyễn Minh Nhật <mjunhan200@gmail.com>
2022-06-20 23:33:45 +0700

    update

---
+++ FE/layouts_FE/footer.php
@@ -0,0 +1,62 @@
+<?php
+    $baseURL ='';
+    require_once ($baseUrl.'./utils/ulitity.php');
+    require_once ($baseUrl.'./database/dbhelper.php');
+    $sql = "SELECT * FROM product WHERE category_id=40 order by updated_at desc limit 0,3";
+    $data = executeResult($sql);
+?>
+<div class="footer">
+    <div class="footer-1">
+        <div class="grid wide footer-items">
+            <div class="row">
+                <div class="col l-3">
+                    <span class="footer-title">LNH-ĐH Điện Lực</span>
+                    <div class="footer-gioithieu">
+                    </div>
+                </div>
+                
+                <div class="col l-3">
+                    <span class="footer-title">SẢN PHẤM ĐÁNH GIÁ CAO</span>
+                    <div class="rate-height">
+                    <?php
+                    foreach($data as $item) {
+                            echo '
+                            <div class="rate-height-items">
+                                <div class="rate-height-box-img">
+                                <a class="tag-a" href="'.$baseUrl.'category/sanpham/sp.php?sp='.$item['id'].'">
+                                    <img style="width: 70;height: 70px;object-fit: cover;" class="product-selling-img" src="'.$baseUrl.fixUrl($item['thumnail'],$roothPath="./").'" alt="">
+                                </a>
+                                </div>
+                                <div class="rate-height-right">
+                                    <span>'.$item['title'].'</span>
+                                    <span class="rate-height-price">'.number_format( $item['discount']).'<sup>đ</sup></span>
+                                </div>
+                            </div>
+                            ';
+                        }
+                    ?>
+                    </div>
+                </div>
+                <div class="col l-3">
+                    <span class="footer-title">THÔNG TIN VÀ HƯỚNG DẪN</span>
+                    <a href="<?=$baseUrl.'FE/giaohang'?>" class="tag-a"><div class="footer-inform">Giao hàng và thanh toán</div></a>
+                    <a href="<?=$baseUrl.'FE/baohanh'?>" class="tag-a"><div class="footer-inform">Chế độ bảo hành</div></a>
+                    <a href="<?=$baseUrl.'FE/nhanvadoi'?>" class="tag-a"><div class="footer-inform">Nhận hàng và đổi trả</div></a>
+                    <a href="<?=$baseUrl.'FE/daphop'?>" class="tag-a"><div class="footer-inform">Đập hộp và nhận quà</div></a>
+                    <a href="<?=$baseUrl.'FE/baomat'?>" class="tag-a"><div class="footer-inform-last">Chính sách bảo mật</div></a>
+                </div>
+                <div class="col l-3">
+                    <span class="footer-title">ĐỊA CHỈ CỬA HÀNG</span>
+                    <div class="footer-address">234 Hoàng Quốc Việt, Bắc Từ Liêm, Hà Nội</div>
+                    <div class="footer-address">Đinh Duy Long</div>
+                    <div class="footer-address">Nguyễn Minh Nhật</div>
+                    <div class="footer-address">Đỗ Minh Hoàn</div>
+                </div>
+            </div>
+            
+        </div>
+        <div class="footer-end">
+            <span>Copyright 2022 © Bản quyến thuộc về LNH</span>
+        </div>
+    </div>
+</div>
---
+++ category/lienhe/index.php
@@ -0,0 +1,123 @@
+<?php
+	$title = 'Liên hệ';
+	$baseUrl = '../../';
+    include_once ($baseUrl.'FE/layouts_FE/header.php');
+?>
+<title>LIÊN HỆ</title>
+<style>
+        .wide-nav-items-span {
+            color: #777;
+        }
+        .danhmuc-lienhe {
+            color: #000;
+        }
+</style>
+<div class="lienhe-box">
+    <div class="lienhe-body">
+        <div class="lienhe-top-box">
+            <div class="lienhe-top">
+                <div class="lienhe-connect">
+                    <div class="lienhe-connect-title">
+                        <span>Hãy Kết Nối Với Chúng Tôi</span>
+                    </div>
+                    <div class="lienhe-connect-icon-box">
+                        <div class="lienhe-connect-icon lienheFB">
+                            <a href="" class="tag-a"><i class="fab fa-facebook-f"></i></a>
+                            <div class="header-social-hover lienhe-connect-icon-hover">
+                                <span>Follow on Facebook</span>
+                                <div class="hover-square lienhe-connect-icon-square"></div>
+                            </div>
+                        </div>
+                        <div class="lienhe-connect-icon lienhe-ITG">
+                            <a href="" class="tag-a"><i class="fab fa-instagram"></i></a>
+                            <div class="header-social-hover lienhe-connect-icon-hover">
+                                <span>Follow on Instagam</span>
+                                <div class="hover-square lienhe-connect-icon-square"></div>
+                            </div>
+                        </div>
+                        <div class="lienhe-connect-icon lienhe-TWWITER ">
+                            <a href="" class="tag-a"><i class="fab fa-twitter"></i></a>
+                            <div class="header-social-hover lienhe-connect-icon-hover">
+                                <span>Follow on Twitter</span>
+                                <div class="hover-square lienhe-connect-icon-square"></div>
+                            </div>
+                        </div>
+                        <div class="lienhe-connect-icon lienhe-PHONE ">
+                            <a href="" class="tag-a"><i class="fas fa-phone-alt"></i></a>
+                            <div class="header-social-hover lienhe-connect-icon-hover lienhe-connect-icon-hover-phone">
+                                <span>0378208856</span>
+                                <div class="hover-square lienhe-connect-icon-square"></div>
+                            </div>
+                        </div>
+                        <div class="lienhe-connect-icon lienhe-YT ">
+                            <a href="" class="tag-a"><i class="fab fa-youtube"></i></a>
+                            <div class="header-social-hover lienhe-connect-icon-hover">
+                                <span>Follow on Youtube</span>
+                                <div class="hover-square lienhe-connect-icon-square"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="lienhe-mid">
+            <div class="lienhe-mid-items">
+                <div class="lienhe-mid-img-box">
+                    <img src="<?=$baseUrl?>FE/img/lienhe-mid1.jpg" alt="">
+                </div>
+                <div class="lienhe-mid-city">
+                    <span>HÀ NỘI</span>
+                </div>
+                <div class="lienhe-mid-street">
+                    <span>Số 121 đường Cầu Giấy</span>
+                </div>
+                <div class="lienhe-mid-district">
+                    <span>Quận Cầu Giấy, Thành Phố Hà Nội</span>
+                </div>
+                <div class="lienhe-mid-phone">
+                    <span>+ SĐT: 0996693396</span> 
+                </div>
+                <div class="lienhe-mid-email">
+                    <span>+ Email: minhnhat2001@gmail.com</span>
+                </div>
+            </div>
+            <div class="lienhe-mid-items">
+                <div class="lienhe-mid-img-box">
+                    <img src="<?=$baseUrl?>FE/img/lienhe-mid2.jpg" alt="">
+                </div>
+                <div class="lienhe-mid-city">
+                    <span> Hà Nội</span>
+                </div>
+                <div class="lienhe-mid-street">
+                    <span>234 Hoàng Quốc Việt, Bắc Từ Liêm, Hà Nội</span>
+                </div>
+                <div class="lienhe-mid-district">
+                    <span>234 Hoàng Quốc Việt, Bắc Từ Liêm, Hà Nội</span>
+                </div>
+                <div class="lienhe-mid-phone">
+                    <span>+ SĐT: 091.345.2502</span> 
+                </div>
+                <div class="lienhe-mid-email">
+                    <span>+ Email: minhnhat2001@gmail.com</span>
+                </div>
+            </div>
+        </div>
+        <div class="lienhe-bottom-box">
+            <div class="lienhe-bottom">
+                <div class="lienhe-address-title">
+                    <hr class="lienhe-hr">
+                    <span>VỊ TRÍ CỬA HÀNG TRÊN BẢN ĐỒ</span>
+                    <hr class="lienhe-hr">
+                </div>
+                <div class="lienhe-map">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.660774286732!2d105.7844748148546!3d21.046255085988903!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab325c9d85ff%3A0x92f275da77aa7354!2zMjM0IEhvw6BuZyBRdeG7kWMgVmnhu4d0LCBD4buVIE5odeG6vywgVOG7qyBMacOqbSwgSMOgIE7hu5lpLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1655617073200!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+	require_once($baseUrl.'FE/layouts_FE/footer.php');
+?>
+<script src="<?=$baseUrl?>FE/js/cuahang.js"></script>
+<script src="<?=$baseUrl?>FE/js/index.js"></script>
